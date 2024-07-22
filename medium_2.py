ac10d1c05700a3f973bde38d584d33d04d1f4372
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-07-22 10:38:07 -0700

    Create medium_2.py

---
+++ medium_2.py
@@ -0,0 +1,11 @@
+# Contains Duplicates
+# class Solution(object):
+#     def containsDuplicate(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: bool
+#         """
+#         new = set(nums)
+#         if len(new)==len(nums):
+#             return False
+#         return True
