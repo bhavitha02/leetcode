e4fad92838bc34cf127c081ed972d3bc24d7b2fa
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-07-22 10:38:57 -0700

    Update medium_2.py

--- medium_2.py
+++ medium_2.py
@@ -1,11 +1,11 @@
 # Contains Duplicates
-# class Solution(object):
-#     def containsDuplicate(self, nums):
-#         """
-#         :type nums: List[int]
-#         :rtype: bool
-#         """
-#         new = set(nums)
-#         if len(new)==len(nums):
-#             return False
-#         return True
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        new = set(nums)
+        if len(new)==len(nums):
+            return False
+        return True
