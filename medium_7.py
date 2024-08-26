2538c27c457ddcd1f5f9b6deb9bfdc0b32767cae
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-08-26 09:52:45 -0700

    Create medium_7.py

---
+++ medium_7.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/word-break/description/
+ class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        start = 0
+        count = 0
+        for i in wordDict:
+            word_len =  start + len(i)
+            if i == s[start:word_len]:
+                start = word_len
+                count += 1
+            else:
+                return False
+            if count == len(wordDict):
+                return True
