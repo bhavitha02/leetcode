7b0e23a44ca12befb25c834d1f53c48357771262
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-08-29 14:20:14 -0700

    Create medium_11.py

---
+++ medium_11.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/unique-paths/description/
+
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        No idea 
