cf70727ed31081799d361fd3a148624e9e662487
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-09-04 17:23:03 -0700

    Create medium_17.py

---
+++ medium_17.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/clone-graph/description/
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        
