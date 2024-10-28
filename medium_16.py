afac5b4ab425ec5f0d3d0b6b35a846209473128d
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-10-28 14:37:31 -0700

    Update medium_16.py

--- medium_16.py
+++ medium_16.py
@@ -1,20 +1,12 @@
 # https://leetcode.com/problems/merge-intervals/description/
 class Solution(object):
     def merge(self, intervals):
-        """
-        :type intervals: List[List[int]]
-        :rtype: List[List[int]]
-        """
-        lst = [intervals[0]]
-        for i in range(len(intervals)-1):
-            if lst[-1][1] >= intervals[i+1][0] and lst[-1][0] <= intervals[i+1][0]:
-                lst[-1][1] = intervals[i+1][1]
-            elif lst[-1][1] >= intervals[i+1][0] and lst[-1][0] > intervals[i+1][0] and lst[-1][1] <= intervals[i+1][1]:
-                lst[-1][1] = intervals[i+1][1]
-                lst[-1][0] = intervals[i+1][0]
-            elif lst[-1][1] >= intervals[i+1][0] and lst[-1][0] > intervals[i+1][0] and lst[-1][1] >= intervals[i+1][1]:
-                lst[-1][0] = intervals[i+1][0]
-                
+        intervals = sorted(intervals,key=lambda x:x[0])
+        output_list = [intervals[0]]
+        for i in intervals[1:]:
+            if i[0]<=output_list[-1][1]:
+                if i[1]>output_list[-1][1]:
+                    output_list[-1][1] = i[1]
             else:
-                lst.append(intervals[i+1])
-        return lst
+                output_list.append(i)
+        return output_list
