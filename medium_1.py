c1849d5ca4b7523416a4e3e4e2b0e90ba88fcfec
bhavitha02 <79206914+bhavitha02@users.noreply.github.com>
2024-10-28 11:03:30 -0700

    Update medium_1.py

--- medium_1.py
+++ medium_1.py
@@ -1,10 +1,7 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 class Solution(object):
     def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
+        #  Sol 1: O(n) = n^2
         # if len(prices)<=1:
         #     return 0
         # max_value = 0
@@ -17,18 +14,23 @@ class Solution(object):
         #             max_value = diff
                 
         # return max(max_value, 0)
-        # [1,2,4,5,8,9]
-        # [1, 1, 1, 1, 1, 1]
-        # [10,15,7,8,10,1,2,3]
-        # [10,10,10,7,7,7,1,1]
-        mini = [prices[0]]
-        for i in range(1, len(prices)):
-            mini.append(min(mini[-1], prices[i-1]))
-        return max([i-j for i, j in zip(prices, mini)])
-
-        # time complexity is O(n)
-        # space complexity is O(n)
-
+        # Sol 2: O(n) = n 
+          mini = [prices[0]]
+          for i in range(1, len(prices)):
+              mini.append(min(mini[-1], prices[i-1]))
+          return max([i-j for i, j in zip(prices, mini)])
+        # Sol 3: O(n) = n Sliding window technique
+          buy = 0
+          max_profit = 0
+          for sell in range(1, len(prices)):
+              if prices[buy] < prices[sell]:
+                  current_profit = prices[sell] - prices[buy]
+                  max_profit = max(current_profit, max_profit)
+              else:
+                  buy = sell
+          return max_profit
+        
+        
 
 
 
